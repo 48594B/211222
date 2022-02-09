2a400795cb95c7d2c199d1e5ec5ff2fded3aca9a
48594b <48594b@gmail.com>
2022-02-09 23:56:42 +0900

    test

---
+++ 211222/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int Height = 0;
+	int Base = 0;
+
+	float Area = 0.0f;
+
+	cin >> Height;
+	cin >> Base;
+	
+		// 4 + ( 4 / 4 )
+	Area = static_cast<float>(Height) * static_cast<float> (Base) / 2.0f;
+	cout << Area << endl;
+
+	cout << "git »ý¼º" << endl;
+
+	return 0;
+}
+
+
+
